08ec644bf00a2d4a44a0fa83136f4fb64ba4e3ae
Sam Cullen <samcull2000@gmail.com>
2024-05-29 22:33:44 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,3 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
-
 'use strict';
 
 const api_key = "fbafc109b7b0750199a1ed7ed9c63fe3";
